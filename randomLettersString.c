2335f1fd76c497024d7bb289beaee50812eb7dad
root <root@localhost>
2020-03-30 16:44:12 +0200

    neuspesny pokus na napisanie pismen

--- randomLettersString.c
+++ randomLettersString.c
@@ -6,7 +6,7 @@ void npa(char *str, int num)
 	srand(time(NULL));
 	//printf("pismena: %s\nnum: %d\n", str, num); //test
 	char u;
-	int i, j, tahy[26] = {}, rNum, check;
+	int i, j, tahy[26] = {}, rNum, check, k = 97; //k je pre "hodnoty" pismen, i a j pre indexi
 	i = 0;
 	do
 	{
@@ -26,9 +26,12 @@ void npa(char *str, int num)
 		}
 		
 		if (check == 0)
-			i++;
-		
-		printf("i = %d\n", i);
+			{
+				i++;
+				u = k; // priradi pismenko to "u"
+				str = u; // priradi "u" na adresu "str"
+				str++; //posunie adresu "str"
+			}
 		
 	} while (i < num);
 	
@@ -68,19 +71,12 @@ main()
 	cislic, ktore budu potom pozicie v mojom abc poli, budu sa postupne naplnat a kazde cislo
 	bude predstavovat nejake ine pismeno*/
 	
-	for (i = 97; i < 97+numL; i++)
-	{
-		u = i;
-		abc[j] = u;
-		//printf("%s\n", abc);
-		j++;
-	}
-	
 	//printf("cely string: %s\n", abc);
 	
 	//#################TEST PROCEDURY#################\\
 	
 	npa(abc, numL);
+	printf("pismenka: %s\n", abc);
 	
 	printf("task failed successfully\n");
 }
