edf0db32804b5d7ba85dcdd4d7f1022df4619e34
root <root@localhost>
2020-03-30 10:36:07 +0200

    basic concept

--- randomLettersString.c
+++ randomLettersString.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void nahodna_permutacia_abecedy(char *str, int num)
+{
+	
+}
 
 main()
 {
-	char u;
-	int i;
+	int numL, isCorrect = 0;
+	do
+	{
+	printf("pocet prvch malych pismen abecedy: \n");
+	scanf("%d", &numL);
 	
-	for (i = 97; i < 97+26; i++)
+	if (numL > 0 && numL < 27)
+	{
+		isCorrect = 1;
+	} 
+	else
+	{
+		printf("zle zadane cislo\n");
+	}
+	
+	} while (isCorrect == 0); 
+	
+	char abc[26] = {};
+	char u;
+	int i, j = 0;
+	
+	/*BASIC CONCEPT: este tu doplnit algoritmus na loteriu, ktory mi da random poradie numL
+	cislic, ktore budu potom pozicie v mojom abc poli, budu sa postupne naplnat a kazde cislo
+	bude predstavovat nejake ine pismeno*/
+	
+	for (i = 97; i < 97+numL; i++)
 	{
 		u = i;
-		printf("%c\n%d\n", u, u);
+		abc[j] = u;
+		//printf("%s\n", abc);
+		j++;
 	}
+	
+	printf("%s\n", abc);
+	
+	
 }
