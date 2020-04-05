0dff19cf35cde9d2541104acee46754bf0fbbc24
root <root@localhost>
2020-04-05 22:43:57 +0200

    totalRemake: vytvorená pomocná funkcia

--- randomLettersString.c
+++ randomLettersString.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void vymen(char *str, int num)
+{
+	int x, y;
+	do							// aby som nedostal rovnake hodnoty
+	{
+		x = rand()%num;
+		y = rand()%num;
+	} while (x == y);
+	
+	printf("x = %d\ny = %d\n", x, y);
+	char u;
+	
+	u = str[x];
+	str[x] = str[y];
+	str[y] = u;
+}
+
 void npa(char *str, int num)
 {
-	
+	int i, j = 0;
+	char u;
+	for (i = 97; i < 97 + num; i++)
+	{
+		u = i;
+		str[j] = u;
+		j++;
+	}
 }
 
 main()
@@ -28,13 +52,13 @@ main()
 	} while (isCorrect == 0); 
 	
 	char abc[26] = {};
-	char u;
-	int i, j = 0;
 	
 	//#################TEST PROCEDURY#################\\
 	
 	npa(abc, numL);
 	printf("pismenka: %s\n", abc);
+	vymen(abc, numL);
+	printf("prehadzane pismenka: %s\n", abc);
 	
 	printf("task failed successfully\n");
 }
