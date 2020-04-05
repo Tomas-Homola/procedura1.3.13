6ef99d059c898de29c7a705b6a143a02b8d90ed2
root <root@localhost>
2020-04-05 22:31:56 +0200

    totalRemake: začiatok

--- randomLettersString.c
+++ randomLettersString.c
@@ -3,36 +3,7 @@
 
 void npa(char *str, int num)
 {
-	char u;
-	int i, j, tahy[26] = {}, rNum, check, k = 97; //k je pre "hodnoty" pismen, i a j pre indexi
-	i = 0;
-	do
-	{
-		rNum = rand()%num+0;
-		
-		tahy[i] = rNum;
-		
-		check = 0;
-		
-		for (j = 0; j < i; j++)
-		{
-			if (rNum == tahy[j])
-			{
-				check = 1;
-				continue;
-			}
-		}
-		
-		if (check == 0)
-			{
-				u = k; // priradi pismenko to "u"
-				printf("miesto: %d; char: %c\n", tahy[i], u); //kontrolne vypisovanie
-				str[tahy[i]] = u;
-				k++;
-				i++;
-			}
-		
-	} while (i < num);
+	
 }
 
 main()
@@ -59,9 +30,6 @@ main()
 	char abc[26] = {};
 	char u;
 	int i, j = 0;
-	
-	/*BASIC CONCEPT: pomocou algoritmu na loteriu, ktory mi da random numL
-	cislic bez opakovania -> pozície v stringu, budu nasledovne naplnane jednotlivé pozicie v mojom     stringu (abc), pričom písmená sa budú napĺňať postupne, ale na nahodne miesta v tom stringu*/
 	
 	//#################TEST PROCEDURY#################\\
 	
